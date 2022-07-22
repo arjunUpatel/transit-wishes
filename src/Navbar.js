2a925b1e5c66332df8f4ae3fdc461473c0f2cae5
Arjun Patel <arjunpatel151002@gmail.com>
2022-07-21 22:38:14 -0400

    Progress on navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,17 @@
 import { Outlet, Link } from 'react-router-dom'
+import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <nav className='nav'>
-      <Link to="/">Transit Wishes</Link> |{" "}
-      <Link to="/about">About</Link> |{" "}
-      <Link to="/the-map">The Map</Link> |{" "}
-      <Link to="/make-a-wish">Make a Wish</Link>
+    <nav className='navWords'>
+      <Link id='name' className='navWords' to="/">
+        <img>
+        </img>
+        Transit Wishes
+      </Link>
+      <Link id='about' className='navWords' to="/about">About</Link>
+      <Link id='map' className='navWords' to="/the-map">The Map</Link>
+      <Link id='submit' className='navWords' to="/make-a-wish">Make a Wish</Link>
       <Outlet />
     </nav>
   )
