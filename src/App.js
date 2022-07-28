91c830b8ec57c31a2aa9745bc38b0ce10bd098f7
Arjun Patel <arjunpatel151002@gmail.com>
2022-07-28 00:00:05 -0400

    Home page preliminary layout and color scheme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import './App.css';
 import { Link, Outlet } from 'react-router-dom';
 import React from 'react';
+import logo from './logo.png';
+import Footer from './Footer.js';
+
 
 export default function App() {
   return (
-    <div className="main">
-      <div className="container">
-        <img height={300} width={300} src={require('./logo.png')} />
-        <h1>Transit</h1>
-        <h1 id='wishes'>Wishes</h1>
-        <p>Make your transit needs heard</p>
+    <div className='wrapper'>
+
+      <div className="main">
+        <div className="container">
+          <img height={300} width={300} src={logo} />
+          <h1>Transit</h1>
+          <h1 id='wishes'>Wishes</h1>
+          <p>Make your transit needs heard</p>
+        </div>
       </div>
-      <div className="nav">
-        <Link to="/about">About</Link> {" | "}
-        <Link to="/the-map">The Map</Link>{" | "}
-        <Link to="/make-a-wish">Make a Wish</Link>{" | "}
-        <Link to="/voice-your-needs">Voice your Needs</Link>
-        <Outlet />
+
+      <div className="home-nav">
+        <div className='links'>
+          <Link className='home-link' to="/about">About</Link>
+          <Link className='home-link' to="/the-map">The Map</Link>
+          <Link className='home-link' to="/make-a-wish">Make a Wish</Link>
+          <Link className='home-link' to="/voice-your-needs">Voice your Needs</Link>
+          <Outlet />
+        </div>
       </div>
+
+      <Footer></Footer>
+
     </div>
   );
 }
