471e6293206e2e5c899ee93328e5e1d5614137fa
Arjun Patel <arjunpatel151002@gmail.com>
2022-08-04 20:56:39 -0400

    Google maps markers basic functionality and miscellaneous

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import './App.css';
-import { Link, Outlet } from 'react-router-dom';
 import React from 'react';
 import logo from './images/logo.png';
 import Footer from './Footer.js';
+import Navbar from './Navbar.js'
 
 export default function App() {
   return (
     <>
+      <Navbar />
       <div className="home-container">
-        <img height={300} width={300} src={logo} />
+        <img height={300} width={300} src={logo} alt="Logo" />
         <h1>Transit</h1>
-        <h1 id='wishes'>Wishes</h1>
+        <h1>Wishes</h1>
         <p className='home-para'>Make your transit needs heard</p>
-      </div>
-
-      <div className='home-nav'>
-        <Link className='home-link' to="/about">About</Link>
-        <Link className='home-link' to="/the-map">The Map</Link>
-        <Link className='home-link' to="/make-a-wish">Make a Wish</Link>
-        <Link className='home-link' to="/voice-your-needs">Voice your Needs</Link>
-        <Outlet />
       </div>
       <Footer />
     </>
--- src/Submission.js
+++ src/Submission.js
@@ -1,11 +1,77 @@
 import Navbar from "./Navbar";
-import React from 'react';
+import React, { useState } from 'react';
+import './Submission.css'
+import Footer from "./Footer";
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
+
+function SubmitButton() {
+  const handleClick = () => {
+
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Submit
+    </button>
+  )
+}
 
 export default function Submission() {
+
+  const libraries = ['places']
+  const center = { lat: 40.0583, lng: -74.4057 }
+  const options = {
+    disableDefaultUI: true,
+  }
+  const [markers, setMarkers] = useState([])
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
+    libraries,
+  })
+
+  if (loadError) return <div>Error loading maps</div>
+  if (!isLoaded) return <div>Loading...</div>
   return (
-    <div>
+    <>
       <Navbar></Navbar>
-      <h1>Make a wish</h1>
-    </div>
+      <div className="main">
+        <div className="submission-container">
+          <h1>Make a wish</h1>
+          <h2 className="submission-subheading">The What?</h2>
+          <p className="submission-para">Have an idea where you would like to have public transit connection? Place it on the map by following the steps below!</p>
+          <h2 className="submission-subheading">The How?</h2>
+          <ol>
+            <li>The first marker placed will be the desired pickup point. This will be denoted by a red marker.</li>
+            <li>After confirming the location of the pickup point, you will be able to place the destination marker. This will be denoted by a blue marker.</li>
+            <li>You can select up to connections in one submission. When you are done, press the submit.</li>
+          </ol>
+          <GoogleMap
+            zoom={8.2}
+            center={center}
+            mapContainerClassName='map-container'
+            options={options}
+            onClick={(event) => {
+              setMarkers((current) => [
+                ...current,
+                {
+                  lat: event.latLng.lat(),
+                  lng: event.latLng.lng(),
+                  time: new Date(),
+                }
+              ])
+            }}
+          >
+            {markers.map((marker) => (
+              <Marker
+                key={marker.time}
+                position={{ lat: marker.lat, lng: marker.lng }}
+              />
+            ))}
+          </GoogleMap>
+          <SubmitButton />
+        </div>
+      </div>
+      <Footer />
+    </>
   )
 }
