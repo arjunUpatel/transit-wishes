81bd45c153faaa398d7c5593f194a012005dba75
Arjun Patel <arjunpatel151002@gmail.com>
2022-09-01 16:11:16 -0400

    Map page ui changes and fetching data from database

--- src/Map.js
+++ src/Map.js
@@ -1,33 +1,122 @@
-import { GoogleMap, useLoadScript } from '@react-google-maps/api'
-import Header from './Header';
-import React from 'react';
-import Footer from './Footer';
+import Header from "./Header"
+import React, { useCallback, useRef, useState } from 'react'
+import './Submission.css'
 import './Map.css'
+import { GoogleMap, useLoadScript, Marker, MarkerClusterer } from '@react-google-maps/api'
+import mapStyles from "./mapStyles"
+import Offcanvas from 'react-bootstrap/Offcanvas'
+import infoIcon from './images/information.png'
+import OverlayTrigger from "react-bootstrap/OverlayTrigger"
+import Tooltip from "react-bootstrap/Tooltip"
+import { db } from './firebase-config'
+import { collection, getDocs } from "firebase/firestore"
 
 export default function Map() {
-  const { isLoaded } = useLoadScript({
+
+  const [selected, setSelected] = useState(null)
+  const [info, setInfo] = useState(false)
+
+  const markers = []
+  const startingMarkers = []
+
+  const fetchMarkerData = async () => {
+    const querySnapshot = await getDocs(collection(db, "markers"))
+    querySnapshot.forEach((snapshot) => {
+      const obj = { ...snapshot.data() }
+      const markerPair = []
+      Object.values(obj.latLng).forEach((value) => {
+        markerPair.push({ lat: value.lat, lng: value.lng })
+      })
+      markers.push(markerPair)
+      startingMarkers.push(markerPair[0])
+    })
+  }
+
+  const center = { lat: 40, lng: -99 }
+  const options = {
+    disableDefaultUI: true,
+    styles: mapStyles,
+    disableAutoPan: true,
+    clickableIcons: false,
+  }
+  const mapRef = useRef()
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   })
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map
+    panTo(center, 4.5)
+    fetchMarkerData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-  if (!isLoaded) return <div>Loading...</div>
+  const panTo = useCallback(({ lat, lng }, zoom) => {
+    mapRef.current.panTo({ lat, lng })
+    mapRef.current.setZoom(zoom)
+  }, [])
+
+  function InfoOffcanvas() {
+    return (
+      <Offcanvas show={info} onHide={() => { setInfo(false) }}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Info</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+        </Offcanvas.Body>
+      </Offcanvas>
+    )
+  }
+
+  function InfoButton() {
+
+    const renderTooltip = (props) => (
+      <Tooltip id="button-tooltip" {...props}>
+        Help
+      </Tooltip>
+    )
+
+    return (
+      <OverlayTrigger
+        placement="right"
+        delay={{ show: 200, hide: 300 }}
+        overlay={renderTooltip}>
+        <span className="submission-info-icon" onClick={() => { setInfo(true) }}>
+          <img width={60} height={60} src={infoIcon} alt='Info' />
+        </span>
+      </OverlayTrigger>
+    )
+  }
+
+  if (loadError)
+    return <div>Error loading maps</div>
+  if (!isLoaded)
+    return <div>Loading...</div>
   return (
-    <div>
-      <Header />
-      <div className='main'>
-        <div className='map-content'>
-          <div className='map-text'>
-            <h1>The Map</h1>
-            <h2 className='map-subheading'>How it works?</h2>
-            <ul>
-              <li>By default, the map displays popular pick up points as red clusters.</li>
-              <li>Clicking on a red cluster will reveal popular drop off points as blue clusters.</li>
-              <li>Zooming into clusters will split clusters incrementally and show the location of submissions with a greater accuracy.</li>
-            </ul>
-          </div>
-          <GoogleMap zoom={8.2} center={{ lat: 40.0583, lng: -74.4057 }} mapContainerClassName='map-container' />
-        </div>
+    <>
+      <div id="submission-navbar">
+        <Header />
       </div>
-      <Footer />
-    </div>
-  );
+      <InfoButton />
+      <InfoOffcanvas />
+      <GoogleMap
+        id="google-map"
+        mapContainerClassName='submission-map-container'
+        options={options}
+        onLoad={onMapLoad}
+      >
+        <MarkerClusterer
+          imagePath='https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+        // imageExtension="png"
+        >
+          {(clusterer) => startingMarkers.map((location, i) => (
+            <Marker
+              key={i}
+              position={location}
+              clusterer={clusterer}
+            />
+          ))}
+        </MarkerClusterer>
+      </GoogleMap>
+    </>
+  )
 }
